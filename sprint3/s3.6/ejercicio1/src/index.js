87f95c63fee3623d9c81c6f6b7f9d5901b67f1dd
Loreto Vaquero <loret.vaquero@gmail.com>
2018-07-26 19:41:42 +0200

    Remove 3.6

--- sprint3/s3.6/ejercicio1/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import ItemList from './ItemList';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<ItemList />, document.getElementById('root'));
-registerServiceWorker();
--- sprint3/s3.6/ejercicio2/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Page from './Page';
-import registerServiceWorker from './registerServiceWorker';
-
-ReactDOM.render(<Page />, document.getElementById('root'));
-registerServiceWorker();
