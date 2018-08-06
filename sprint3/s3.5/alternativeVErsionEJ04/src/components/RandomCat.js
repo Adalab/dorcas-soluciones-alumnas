9861853f5ae1760405d41c1c15d1f290549b5fb6
Maria Violeta <mariavioleta1417@gmail.com>
2018-08-07 01:12:13 +0200

    alternative version to 3.5ej04

---
+++ sprint3/s3.5/alternativeVErsionEJ04/src/components/RandomCat.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const getRandomInteger = (maxNumber) => Math.floor(Math.random() * maxNumber);
+const NUMBER_OF_CATS = 10;
+
+class RandomCat extends React.Component {
+  render() {
+    const randomCat = getRandomInteger(NUMBER_OF_CATS);
+    const heightCat = this.props.height;
+    const widthCat = this.props.width;
+    return (
+      heightCat === undefined && widthCat === undefined ? 
+      <a href="http://lorempixel.com">
+        <img src={ `http://lorempixel.com/${200}/${400}/cats/${randomCat}` } alt="Random cat" />
+      </a>
+      :
+        
+          <a href="http://lorempixel.com">
+          <img src={ `http://lorempixel.com/${heightCat}/${widthCat}/cats/${randomCat}`} alt="Random cat" />
+        </a>
+      
+    );}
+   
+    
+  }
+
+
+export default RandomCat;
